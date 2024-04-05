670b87c8a759c3c09465bb7130bb51efc48a648c
ANIKET SRIVASTAVA <93678826+Aniket1601@users.noreply.github.com>
2024-04-05 09:23:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.listen(7000, () => {
-  console.log('Listening on port 7000');
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
 });
 
 app.use('/', productRoute);
