6941cc4c450f3dfa729620122704035d3c099cb9
ANIKET SRIVASTAVA <93678826+Aniket1601@users.noreply.github.com>
2024-04-05 09:28:21 +0530

    Create ProductDetails.js

---
+++ frontend/src/components/ProductDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductDetails({ product }) {
+  return (
+    <div>
+      <h2>{product.name}</h2>
+      {/* Display other product details */}
+    </div>
+  );
+}
+
+export default ProductDetails;
